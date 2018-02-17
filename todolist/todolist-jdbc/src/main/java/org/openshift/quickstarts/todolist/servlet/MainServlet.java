231b1c606e6d55ff4fb1ee7151de8e99df9d33ff
IM-NOT-PHP-PROGRAMMER <35314850+IM-NOT-PHP-PROGRAMMER@users.noreply.github.com>
2018-02-17 18:39:59 +0300

    Update MainServlet.java

--- todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/servlet/MainServlet.java
+++ todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/servlet/MainServlet.java
@@ -23,6 +23,7 @@ public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
+        resp.getWriter().append("There is two chairs...");
         writeHtml(resp.getWriter());
     }
 
